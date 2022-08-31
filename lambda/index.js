9b73efe3b0beffc246c3d564722919a936efd739
Daniel Surendran <visrend@amazon.com>
2022-08-31 21:24:53 +1000

    commit

---
+++ lambda/index.js
@@ -0,0 +1,282 @@
+
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+const https = require('https');
+
+const DOCUMENT_ID = "customVideoResponse";
+
+const datasource = {
+    "videoPlayerTemplateData": {
+        "type": "object",
+        "properties": {
+            "backgroundImage": "",
+            "displayFullscreen": false,
+            "headerTitle": "",
+            "headerSubtitle": "",
+            "logoUrl": "",
+            "videoControlType": "skip",
+            "videoSources": [],
+            "sliderType": "determinate"
+        }
+    }
+};
+
+const createDirectivePayload = (aplDocumentId, dataSources = {}, tokenId = "documentToken") => {
+    return {
+        type: "Alexa.Presentation.APL.RenderDocument",
+        token: tokenId,
+        document: {
+            type: "Link",
+            src: "doc://alexa/apl/documents/" + aplDocumentId
+        },
+        datasources: dataSources
+    }
+};
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome to A P J C Tech Summit 2022 - Kendra Search Workshop.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const SearchKendraIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SearchKendraIntent';
+    },
+    handle(handlerInput) {
+        
+        var text = handlerInput.requestEnvelope.request.intent.slots.searchString.value;
+        
+        const speakOutput = 'Searching on Kendra for,  '+ text + '. Just hold ... ';
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const SearchKendraAPLRequestIntentHandler = {
+    canHandle(handlerInput) {
+        // handle named intent
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SearchKendraAPLRequestIntent';
+    },
+    async handle(handlerInput) {
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            // generate the APL RenderDocument directive that will be returned from your skill
+            // const response = await request('https://u8hjnibuce.execute-api.us-east-1.amazonaws.com/findvideo/orion');
+            var search_text = handlerInput.requestEnvelope.request.intent.slots.searchString.value;
+            const return_url = await getRelevantURL(search_text);
+            console.log("Return Url " + return_url)
+            if( return_url ){
+                datasource['videoPlayerTemplateData']['properties']['videoSources'][0] = return_url;
+                datasource['videoPlayerTemplateData']['properties']['headerTitle'] = "Top match for your search on "+ search_text;
+                datasource['videoPlayerTemplateData']['properties']['headerSubtitle'] = "From Kendra"
+            }
+            else{
+                datasource['videoPlayerTemplateData']['properties']['headerTitle'] = "Sorry no content found for your search on : " + search_text;
+            }
+            const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+            // send out skill response
+            return handlerInput.responseBuilder.getResponse();  
+
+        }
+    }
+};
+
+const getRelevantURL = (searchString) => new Promise((resolve, reject) => {
+    console.log("Logger in promise");
+    const options = {
+        hostname: 'u8hjnibuce.execute-api.us-east-1.amazonaws.com',
+        port: 443,
+        path: '/findvideo/orion',
+        method: 'GET',
+    };
+    console.log("Logger in promise 2" + JSON.stringify(options));
+    try{
+    //const return_url = await doRequest("https://u8hjnibuce.execute-api.us-east-1.amazonaws.com/findvideo/orion")
+        https.get('https://u8hjnibuce.execute-api.us-east-1.amazonaws.com/findvideo/orion', (resp) => {
+          let data = '';
+        
+          // A chunk of data has been received.
+          resp.on('data', (chunk) => {
+            data += chunk;
+            //console.log(data);
+            //resolve(data);
+          });
+        
+          // The whole response has been received. Print out the result.
+          resp.on('end', () => {
+            console.log(data);
+            resolve(data);
+          });
+        
+        }).on("error", (err) => {
+          console.log("Error: " + err.message);
+          reject(err);
+        });
+    }
+    catch (error){
+        console.error(error);
+        reject(error);  
+    }
+    
+});
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        SearchKendraIntentHandler,
+        SearchKendraAPLRequestIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .lambda();
+
+
+
+
